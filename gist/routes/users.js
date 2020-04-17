523e4e4d77545a8206feb0db6d3020d7e7790d71
jinxiaolin <137185065@qq.com>
2020-04-17 12:45:33 +0800

    add:login

--- gist/routes/users.js
+++ gist/routes/users.js
@@ -1,9 +1,54 @@
+const GH_CLIENT_ID = '/** your client id **/';
+const GH_CLIENT_SECRET = '/** your client secret **/';
+const GH_CALLBACK = '/** your callback **/';
+
+require('url-search-params-polyfill');
+const axios = require('axios');
+const querystring = require('querystring');
+
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.send('respond with a resource');
 });
+
+router.get('/login', function(req, res, next) {
+
+    const url = 'https://github.com/login/oauth/authorize' +
+        '?client_id=' + encodeURIComponent(GH_CLIENT_ID) +
+        '&redirect_uri=' + encodeURIComponent(GH_CALLBACK);
+
+    res.render('login', { url });
+});
+
+router.get('/ghcallback', async(req, res) => {
+
+    if (!req.query.code) res.status(500).send('bad code!');
+
+    console.log("in callback");
+
+    let params = new URLSearchParams();
+    params.append("client_id", GH_CLIENT_ID);
+    params.append("client_secret", GH_CLIENT_SECRET);
+    params.append("code", req.query.code);
+    params.append("redirect_uri", GH_CALLBACK);
+
+    const { data } = await axios.post("https://github.com/login/oauth/access_token", params);
+    if (data) {
+
+        const acode = querystring.parse(data).access_token;
+        axios.get('https://api.github.com/user', {
+            params: {
+                access_token: acode
+            }
+        }).then(function(user) {
+            console.log("got user info", user.data);
+            res.json(user.data);
+            res.end();
+        });
+    }
+})
 
 module.exports = router;
